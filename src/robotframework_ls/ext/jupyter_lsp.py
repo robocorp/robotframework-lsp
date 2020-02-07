eaf31ddc42e346d54658c539d07d95ef5982eb43
Nicholas Bollweg <nick.bollweg@gmail.com>
2020-02-07 09:48:20 -0500

    Add binder (#16)
    Add binder support.
    
    Url: https://mybinder.org/v2/gh/robocorp/robotframework-lsp/master?urlpath=lab
    
    If you add a doc you'd like to open (preferably an .ipynb), the urlpath would just become, ?urlpath=lab/tree/docs/index.ipynb

---
+++ src/robotframework_ls/ext/jupyter_lsp.py
@@ -0,0 +1,17 @@
+""" optional jupyter[lab]_lsp integration
+"""
+def spec_v1(app):
+    """ A spec loader for jupyter_lsp
+    """
+    return {
+        "robotframework_ls": dict(
+            version=1,
+            argv=["robotframework_ls"],
+            languages=["robotframework", "plain"],
+            mime_types=["text/x-robotframework"],
+            urls=dict(
+                home="https://github.com/robocorp/robotframework-lsp",
+                issues="https://github.com/robocorp/robotframework-lsp/issues",
+            )
+        )
+    }
--- src/setup.py
+++ src/setup.py
@@ -16,14 +16,14 @@ setup(
     author="Fabio Zadrozny",
     copyright="Robocorp Technologies, Inc.",
     packages=find_packages(exclude=["robotframework_ls_tests",]),
-    
+
     # List run-time dependencies here. These will be installed by pip when
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
     install_requires=[
     ],
-    
+
     # List additional groups of dependencies here (e.g. development
     # dependencies). You can install these using the following syntax,
     # for example:
@@ -42,5 +42,6 @@ setup(
     # pip to create the appropriate form of executable for the target platform.
     entry_points={
         "console_scripts": ["robotframework_ls = robotframework_ls.__main__:main",],
+        "jupyter_lsp_spec_v1": ["robotframework_ls = robotframework_ls.ext.jupyter_lsp:spec_v1"]
     },
 )
