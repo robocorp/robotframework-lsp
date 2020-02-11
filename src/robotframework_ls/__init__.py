9c78224c4aa554597e193d94568eb9b0dcd5e998
Fabio Zadrozny <fabiofz@gmail.com>
2020-02-11 09:42:11 -0300

    Setting new version to 0.0.3

--- src/robotframework_ls/__init__.py
+++ src/robotframework_ls/__init__.py
@@ -4,5 +4,5 @@ from robotframework_ls.constants import IS_PY2
 if IS_PY2:
     fix_py2.fix_py2()
 
-__version__ = "0.0.2"
+__version__ = "0.0.3"
 version_info = [int(x) for x in __version__.split(".")]
