d99122f43750033d2e83e4cdc25bca25eed177d6
Fabio Zadrozny <fabiofz@gmail.com>
2020-03-09 11:13:08 -0300

    Change license to Apache 2.0. Fixes #24

--- tests/robotframework_ls_tests/test_document.py
+++ tests/robotframework_ls_tests/test_document.py
@@ -1,21 +1,21 @@
-############################################################################
-# Original work Copyright 2018 Palantir Technologies, Inc.                 #
-# Original work licensed under the MIT License.                            #
-# See ThirdPartyNotices.txt in the project root for license information.   #
-# All modifications Copyright (c) Open Law Library. All rights reserved.   #
-#                                                                          #
-# Licensed under the Apache License, Version 2.0 (the "License")           #
-# you may not use this file except in compliance with the License.         #
-# You may obtain a copy of the License at                                  #
-#                                                                          #
-#     http: // www.apache.org/licenses/LICENSE-2.0                         #
-#                                                                          #
-# Unless required by applicable law or agreed to in writing, software      #
-# distributed under the License is distributed on an "AS IS" BASIS,        #
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. #
-# See the License for the specific language governing permissions and      #
-# limitations under the License.                                           #
-############################################################################
+# Original work Copyright 2017 Palantir Technologies, Inc. (MIT)
+# Original work Copyright 2020 Open Law Library. (Apache 2)
+# See ThirdPartyNotices.txt in the project root for license information.
+# All modifications Copyright (c) Robocorp Technologies Inc.
+# All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License")
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http: // www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 from robotframework_ls.workspace import Document
 from robotframework_ls.lsp import TextDocumentContentChangeEvent, Position, Range
 from robotframework_ls import uris
