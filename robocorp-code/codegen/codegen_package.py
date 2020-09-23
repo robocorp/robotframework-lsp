ed8080a62b4d5933423327e83d08fddf6cdb2092
Fabio Zadrozny <fabiofz@gmail.com>
2020-09-23 14:28:22 -0300

    Mark Robocorp Code as requiring robotframework-lsp in the VSCode marketplace. Fixes #147

--- robocorp-code/codegen/codegen_package.py
+++ robocorp-code/codegen/codegen_package.py
@@ -51,6 +51,7 @@ def get_json_contents():
         "icon": "images/icon.png",
         "publisher": "robocorp",
         "engines": {"vscode": "^1.43.0"},
+        "extensionDependencies": ["robocorp.robotframework-lsp"],
         "categories": [],
         "activationEvents": get_activation_events_for_json(),
         "contributes": {
