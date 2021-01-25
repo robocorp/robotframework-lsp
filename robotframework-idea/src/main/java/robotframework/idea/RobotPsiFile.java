1f71fa63b01f220fc54acacc8bc3f516c78495e2
Fabio Zadrozny <fabiofz@gmail.com>
2021-01-25 09:47:38 -0300

    Lots of intellij-related scaffolding to get it to match the defined language on code-completion.

---
+++ robotframework-idea/src/main/java/robotframework/idea/RobotPsiFile.java
@@ -0,0 +1,18 @@
+package robotframework.idea;
+
+import com.intellij.extapi.psi.PsiFileBase;
+import com.intellij.lang.Language;
+import com.intellij.openapi.fileTypes.FileType;
+import com.intellij.psi.FileViewProvider;
+import org.jetbrains.annotations.NotNull;
+
+public class RobotPsiFile extends PsiFileBase {
+    protected RobotPsiFile(@NotNull FileViewProvider viewProvider, @NotNull Language language) {
+        super(viewProvider, language);
+    }
+
+    @Override
+    public @NotNull FileType getFileType() {
+        return RobotFrameworkFileType.INSTANCE;
+    }
+}
