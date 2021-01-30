b0ecf1cdf4a7af77c7f8a8b98af0adcc196de16b
Fabio Zadrozny <fabiofz@gmail.com>
2021-01-30 10:31:36 -0300

    Intellij: comment/uncomment

---
+++ robotframework-idea/src/main/java/robotframework/idea/RobotCommenter.java
@@ -0,0 +1,31 @@
+package robotframework.idea;
+
+import com.intellij.lang.Commenter;
+import org.jetbrains.annotations.Nullable;
+
+public class RobotCommenter implements Commenter {
+    @Override
+    public @Nullable String getLineCommentPrefix() {
+        return "#";
+    }
+
+    @Override
+    public @Nullable String getBlockCommentPrefix() {
+        return null;
+    }
+
+    @Override
+    public @Nullable String getBlockCommentSuffix() {
+        return null;
+    }
+
+    @Override
+    public @Nullable String getCommentedBlockCommentPrefix() {
+        return null;
+    }
+
+    @Override
+    public @Nullable String getCommentedBlockCommentSuffix() {
+        return null;
+    }
+}
