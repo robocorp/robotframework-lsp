97b6f20266a7d45d4bfa62e95ed2e53bd17d44ea
Marduk Bolaños <mardukbp@mac.com>
2021-07-25 12:51:20 +0200

    Made all methods (except complete) private. Added type hints. Extracted the constants to the global scope.

--- robotframework-ls/src/robotframework_ls/impl/dictionary_completions.py
+++ robotframework-ls/src/robotframework_ls/impl/dictionary_completions.py
@@ -1,7 +1,8 @@
 import re
+from typing import Dict, List, Tuple
 from robot.api import Token
 from robocorp_ls_core.robotframework_log import get_logger
-from robotframework_ls.impl import ast_utils
+from robotframework_ls.impl.protocols import ICompletionContext
 from robocorp_ls_core.lsp import (CompletionItem,
                                   MarkupKind,
                                   CompletionItemKind,
@@ -11,59 +12,65 @@ from robocorp_ls_core.lsp import (CompletionItem,
                                   TextEdit)
 
 log = get_logger(__name__)
+_DICT_VARIABLE_REGEX = re.compile(r"[$|&]{(\w+)}")
+_DICT_KEY_REGEX = re.compile(r"\[([\w\s]*)\]")
 
 
-def get_variables(completion_context):
+def _get_variables(completion_context: ICompletionContext):
     variables = []
-    ast = completion_context.get_ast()
-    for node_info in ast_utils.iter_variables(ast):
+    for node_info in completion_context.get_all_variables():
         node = node_info.node
-        var_name = node.get_token(Token.VARIABLE).value
+        token = node.get_token(Token.VARIABLE)
+        if token is None:
+            continue
+        var_name = token.value
         var_value = node.value
         variables.append((var_name, var_value))
     return variables
 
 
-def get_dict_name(dict_item_access):
-    dict_variable_regex = re.compile(r"[$|&]{(\w+)}")
-    dict_name = dict_variable_regex.findall(dict_item_access)
+def _get_dict_name(dict_item_access: str):
+    dict_name = _DICT_VARIABLE_REGEX.findall(dict_item_access)
     return "&{" + dict_name.pop() + "}"
 
 
-def get_dict_keys(dict_item_access):
-    dict_key_regex = re.compile(r"\[([\w\s]*)\]")
-    dict_keys = dict_key_regex.findall(dict_item_access)
+def _get_dict_keys(dict_item_access: str):
+    dict_keys = _DICT_KEY_REGEX.findall(dict_item_access)
     dict_keys.reverse()
     return dict_keys
 
 
-def get_dictionary(variables, dict_name, dict_keys):
+def _get_dictionary(variables: List[Tuple[str, List[str]]], dict_name: str, dict_keys: List[str]):
     """
     Get the dictionary whose keys are the autocompletion options
     ${dict_name}([dict_key])*[<dictionary.keys()>]
     """
     for var_name, var_value in variables:
         if var_name.startswith(dict_name):
-            dictionary = as_dictionary(var_value)
+            dictionary = _as_dictionary(var_value)
             dict_entry = dict_keys.pop()
             if dict_entry == '':
                 return dictionary
             else:
                 dict_variable = dictionary[dict_entry]
-                dict_name = get_dict_name(dict_variable)
-                dict_keys += get_dict_keys(dict_variable)
-                return get_dictionary(variables, dict_name, dict_keys)
+                dict_name = _get_dict_name(dict_variable)
+                dict_keys += _get_dict_keys(dict_variable)
+                return _get_dictionary(variables, dict_name, dict_keys)
     return None
 
 
-def as_dictionary(dict_tokens):
+def _as_dictionary(dict_tokens: List[str]):
     """
     Parse ["key1=val1", "key2=val2",...] as a dictionary
     """
-    return dict([token.split("=") for token in dict_tokens])
+    dictionary = {}
+    for token in dict_tokens:
+        key, val = token.split("=")
+        dictionary.update({key: val})
+    return dictionary
 
 
-def completion_items(dictionary, editor_range):
+def _completion_items(dictionary: Dict[str, str], editor_range: Range):
     return [CompletionItem(
              key,
              kind=CompletionItemKind.Variable,
@@ -75,23 +82,20 @@ def completion_items(dictionary, editor_range):
              ).to_dict() for key, value in dictionary.items()]
 
 
-def complete(completion_context):
-    """
-    :param CompletionContext completion_context:
-    """
+def complete(completion_context: ICompletionContext):
     token_info = completion_context.get_current_variable()
     if token_info is None:
         return []
     token = token_info.token
     if not token.value.endswith("[]"):
         return []
-    variables = get_variables(completion_context)
+    variables = _get_variables(completion_context)
     for resource_doc in completion_context.get_resource_imports_as_docs():
         new_ctx = completion_context.create_copy(resource_doc)
-        variables += get_variables(new_ctx)
-    dict_name = get_dict_name(token.value)
-    dict_keys = get_dict_keys(token.value)
-    dictionary = get_dictionary(variables, dict_name, dict_keys)
+        variables += _get_variables(new_ctx)
+    dict_name = _get_dict_name(token.value)
+    dict_keys = _get_dict_keys(token.value)
+    dictionary = _get_dictionary(variables, dict_name, dict_keys)
     if dictionary is None:
         return []
     selection = completion_context.sel
@@ -99,4 +103,4 @@ def complete(completion_context):
         start=Position(selection.line, token.col_offset + len(token.value) - len("]")),
         end=Position(selection.line, token.end_col_offset),
     )
-    return completion_items(dictionary, editor_range)
+    return _completion_items(dictionary, editor_range)
