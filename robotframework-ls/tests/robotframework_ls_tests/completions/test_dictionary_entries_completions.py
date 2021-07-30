98d8de4458d370ab3f99c10eccc02b6feac34ebb
Marduk Bolaños <mardukbp@mac.com>
2021-07-31 00:59:09 +0200

    Fixed the regex to match dictionary variables with spaces in their name

--- robotframework-ls/tests/robotframework_ls_tests/completions/test_dictionary_entries_completions.py
+++ robotframework-ls/tests/robotframework_ls_tests/completions/test_dictionary_entries_completions.py
@@ -66,3 +66,28 @@ Dictionary Variable
         CompletionContext(doc, workspace=workspace.ws, line=line, col=col - len("]"))
     )
     data_regression.check(completions)
+
+
+def test_dictionary_entries_completions_4(
+    workspace, libspec_manager, data_regression
+):
+    from robotframework_ls.impl.completion_context import CompletionContext
+    from robotframework_ls.impl import dictionary_completions
+
+    workspace.set_root("case1", libspec_manager=libspec_manager)
+    doc = workspace.get_doc("case1.robot")
+    doc.source = """
+*** Variables ***
+&{Some Person}         Address=&{home_address}
+&{home_address}   City=&{Finnland}[Cities]   Zip Code=12345
+&{Finnland}       Cities=&{kaupungeista}
+&{kaupungeista}   Home of Aalto University=Espoo
+
+*** Test Cases ***
+Dictionary Variable
+    Log to Console    ${Some Person}[Address][City][Home of]"""
+    line, col = doc.get_last_line_col()
+    completions = dictionary_completions.complete(
+        CompletionContext(doc, workspace=workspace.ws, line=line, col=col - len("]"))
+    )
+    data_regression.check(completions)
