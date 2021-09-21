1beaaa5926787af9227908c69f404eb6df4e179f
Fabio Zadrozny <fabiofz@gmail.com>
2021-09-21 08:36:10 -0300

    RobotFramework Language Server Release 0.23.1

--- robotframework-ls/src/setup.py
+++ robotframework-ls/src/setup.py
@@ -106,7 +106,7 @@ def collect_other_vendored_files():
 
 setup(
     name="robotframework-lsp",
-    version="0.23.0",
+    version="0.23.1",
     description="Language Server Protocol implementation for Robot Framework",
     long_description=_readme.read_text(),
     url="https://github.com/robocorp/robotframework-lsp",
