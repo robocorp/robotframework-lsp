1e563d37a6ffc2300550f930426ffb7cbb226027
Matti Mokko <matti@robocorp.com>
2021-10-07 11:56:42 +0300

    Update RCC to v11.3.2
    RCC version that supports dynamic templates from remote location.
    Enables getting updated templates without making a new release with a
    new RCC version.

--- robocorp-code/vscode-client/src/rcc.ts
+++ robocorp-code/vscode-client/src/rcc.ts
@@ -108,7 +108,7 @@ async function downloadRcc(
             relativePath = "/linux32/rcc";
         }
     }
-    const RCC_VERSION = "v11.2.0";
+    const RCC_VERSION = "v11.3.2";
     const prefix = "https://downloads.robocorp.com/rcc/releases/" + RCC_VERSION;
     const url: string = prefix + relativePath;
 
