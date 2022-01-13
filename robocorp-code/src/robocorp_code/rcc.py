6f647428bc930d470e247f3639606d4c4c43e46c
Fabio Zadrozny <fabiofz@gmail.com>
2022-01-13 11:05:21 -0300

    Updated RCC to v11.6.3

--- robocorp-code/src/robocorp_code/rcc.py
+++ robocorp-code/src/robocorp_code/rcc.py
@@ -69,7 +69,7 @@ def download_rcc(location: str, force: bool = False) -> None:
                     else:
                         relative_path = "/linux32/rcc"
 
-                RCC_VERSION = "v11.5.5"
+                RCC_VERSION = "v11.6.3"
                 prefix = f"https://downloads.robocorp.com/rcc/releases/{RCC_VERSION}"
                 url = prefix + relative_path
 
