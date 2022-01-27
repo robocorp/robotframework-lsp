03af40f121e53ec522f6bc75ad034cf099c51fc2
Fabio Zadrozny <fabiofz@gmail.com>
2022-01-27 09:56:20 -0300

    Lint

--- robotframework-ls/vscode-client/src/escape.ts
+++ robotframework-ls/vscode-client/src/escape.ts
@@ -2,7 +2,7 @@ export function jsonEscapeUTF(str: string) {
     let result = "";
     for (let i = 0; i < str.length; i++) {
         let ch = str.charCodeAt(i);
-        
+
         if (ch < 128) {
             // i.e.: escape any char which isn't ascii.
             result += str.charAt(i);
