e3f40cd6063d9c4c9c422a424bf7151d2a304c84
Fabio Zadrozny <fabiofz@gmail.com>
2022-02-15 09:10:45 -0300

    Check that REST conversion works on Robot Framework 3 too.

--- robotframework-ls/tests/robotframework_ls_tests/test_rest_to_markdown.py
+++ robotframework-ls/tests/robotframework_ls_tests/test_rest_to_markdown.py
@@ -1,3 +1,29 @@
+def test_rest_to_markdown_code_block():
+    from robotframework_ls.impl.robot_specbuilder import _rest_to_markdown
+
+    initial = """
+Do something with rest.
+        
+.. code-block:: robotframework
+
+    FOR    ${a}   IN    @{b}
+        Do Something
+    END
+"""
+
+    assert (
+        _rest_to_markdown(initial)
+        == """Do something with rest.
+
+```robotframework
+FOR    ${a}   IN    @{b}
+    Do Something
+END
+```
+"""
+    )
+
+
 def test_rest_to_markdown():
     from robotframework_ls.impl.robot_specbuilder import _rest_to_markdown
 
