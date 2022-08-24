e88f0b3db61c420bf2d014b536dbb0e8fab36118
Fabio Zadrozny <fabiofz@gmail.com>
2022-08-24 09:13:53 -0300

    [Intellij] Force file association (shouldn't be needed, but this works around complains that it becomes deassociated because the user had another extension). Fixes #736 #605

--- robotframework-intellij/src/main/java/robocorp/robot/intellij/RobotFrameworkFileType.java
+++ robotframework-intellij/src/main/java/robocorp/robot/intellij/RobotFrameworkFileType.java
@@ -1,41 +1,52 @@
 package robocorp.robot.intellij;
 
+import com.intellij.openapi.application.ApplicationManager;
+import com.intellij.openapi.fileTypes.FileTypeManager;
+import com.intellij.openapi.fileTypes.FileTypeRegistry;
 import com.intellij.openapi.fileTypes.LanguageFileType;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
+import robocorp.lsp.intellij.EditorUtils;
 
 import javax.swing.*;
 
 public class RobotFrameworkFileType extends LanguageFileType {
 
-  public static final RobotFrameworkFileType INSTANCE = new RobotFrameworkFileType();
+    public static final RobotFrameworkFileType INSTANCE = new RobotFrameworkFileType();
 
-  private RobotFrameworkFileType() {
-    super(RobotFrameworkLanguage.INSTANCE);
-  }
+    private RobotFrameworkFileType() {
+        super(RobotFrameworkLanguage.INSTANCE);
+        ApplicationManager.getApplication().invokeLater(() -> {
+            EditorUtils.runWriteAction(() -> {
+                FileTypeManager instance = (FileTypeManager) FileTypeRegistry.getInstance();
+                instance.associateExtension(RobotFrameworkFileType.INSTANCE, "resource");
+                instance.associateExtension(RobotFrameworkFileType.INSTANCE, "robot");
+            });
+        });
+    }
 
-  @NotNull
-  @Override
-  public String getName() {
-    return "Robot Framework";
-  }
+    @NotNull
+    @Override
+    public String getName() {
+        return "Robot Framework";
+    }
 
-  @NotNull
-  @Override
-  public String getDescription() {
-    return "Robot Framework";
-  }
+    @NotNull
+    @Override
+    public String getDescription() {
+        return "Robot Framework";
+    }
 
-  @NotNull
-  @Override
-  public String getDefaultExtension() {
-    return "robot";
-  }
+    @NotNull
+    @Override
+    public String getDefaultExtension() {
+        return "robot";
+    }
 
-  @Nullable
-  @Override
-  public Icon getIcon() {
-    return RobotFrameworkIcons.FILE;
-  }
+    @Nullable
+    @Override
+    public Icon getIcon() {
+        return RobotFrameworkIcons.FILE;
+    }
 
 }
