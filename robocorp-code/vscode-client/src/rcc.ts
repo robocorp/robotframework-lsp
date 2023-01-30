04ae67c47235124ed5bdbac49389f8de1a2995bc
Cosmin G. Popa <cosmin.gabriel.popa@gmail.com>
2023-01-30 13:12:37 +0200

    Bumping to the new version of the inspector

--- robocorp-code/vscode-client/src/rcc.ts
+++ robocorp-code/vscode-client/src/rcc.ts
@@ -136,16 +136,16 @@ function getBaseAsZipBasename() {
     if (process.platform == "win32") {
         if (process.arch === "x64" || process.env.hasOwnProperty("PROCESSOR_ARCHITEW6432")) {
             // Check if node is a 64 bit process or if it's a 32 bit process running in a 64 bit processor.
-            basename = "a92bc72c00906499_windows_amd64.zip";
+            basename = "fb3fbb7fab42b8e1_windows_amd64.zip";
         } else {
             throw new Error("Currently only Windows amd64 is supported.");
         }
     } else if (process.platform == "darwin") {
-        basename = "1a730cb686614682_darwin_amd64.zip";
+        basename = "c5e0ca812e531d77_darwin_amd64.zip";
     } else {
         // Linux
         if (process.arch === "x64") {
-            basename = "ce368a68cc436a3a_linux_amd64.zip";
+            basename = "73b35a0612654f6f_linux_amd64.zip";
         } else {
             throw new Error("Currently only Linux amd64 is supported.");
         }
