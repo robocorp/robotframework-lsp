18e6619e9bc12c1bd94640aa8bc4be574080f7a3
Cosmin G. Popa <cosmin.gabriel.popa@gmail.com>
2023-03-13 19:22:04 +0200

    Bump inspector to v0.9.1

--- robocorp-code/vscode-client/src/rcc.ts
+++ robocorp-code/vscode-client/src/rcc.ts
@@ -142,16 +142,16 @@ function getBaseAsZipBasename() {
     if (process.platform == "win32") {
         if (process.arch === "x64" || process.env.hasOwnProperty("PROCESSOR_ARCHITEW6432")) {
             // Check if node is a 64 bit process or if it's a 32 bit process running in a 64 bit processor.
-            basename = "b6dd462cc0c43d95_windows_amd64.zip";
+            basename = "978947424da5b5d4_windows_amd64.zip";
         } else {
             throw new Error("Currently only Windows amd64 is supported.");
         }
     } else if (process.platform == "darwin") {
-        basename = "ef1ea2a4fd833402_darwin_amd64.zip";
+        basename = "05466b72750b50bb_darwin_amd64.zip";
     } else {
         // Linux
         if (process.arch === "x64") {
-            basename = "837b3d29c4e2e5d7_linux_amd64.zip";
+            basename = "21263ad0be4ba1d6_linux_amd64.zip";
         } else {
             throw new Error("Currently only Linux amd64 is supported.");
         }
