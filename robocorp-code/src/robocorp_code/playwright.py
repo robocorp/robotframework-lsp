ca814c4bf2b3678bcd1e0978de69478312ea7879
Cosmin G. Popa <cosmin.gabriel.popa@gmail.com>
2023-07-06 18:00:14 +0300

    Switching implementation to seperate files

--- robocorp-code/src/robocorp_code/playwright.py
+++ robocorp-code/src/robocorp_code/playwright.py
@@ -1,3 +1,4 @@
+from typing import List
 from robocorp_code import commands
 from robocorp_ls_core.command_dispatcher import _SubCommandDispatcher
 from robocorp_ls_core.robotframework_log import get_logger
@@ -31,9 +32,9 @@ class _Playwright(object):
     def _open_playwright_recorder(self, params=None) -> ActionResultDict:
         from robocorp_ls_core.ep_resolve_interpreter import EPResolveInterpreter
         from robocorp_ls_core.ep_resolve_interpreter import IInterpreterInfo
+        from robocorp_ls_core import uris
 
         try:
-            from robocorp_ls_core import uris
 
             target_robot = params.get("target_robot")
             log.debug("Selected robot:", target_robot)
@@ -45,35 +46,12 @@ class _Playwright(object):
                     )
                 )
                 if interpreter_info is not None:
-                    from subprocess import check_output
-
-                    python_path = interpreter_info.get_python_exe()
-                    log.debug("Found python interpreter:", python_path)
-
-                    # make sure we have the drivers installed
-                    log.debug("Installing playwright drivers...")
-                    cmd = [python_path, "-m", "playwright", "install"]
-                    b_output = check_output(cmd, timeout=35)
-                    output = b_output.decode("utf-8", "replace")
-
-                    # open the playwright recorder
-                    log.debug("Opening playwright recorder...")
-                    cmd = [
-                        python_path,
-                        "-m",
-                        "playwright",
-                        "codegen",
-                        "demo.playwright.dev/todomvc",
-                    ]
-                    b_output = check_output(cmd, timeout=35)
-                    output += b_output.decode("utf-8", "replace")
-                    log.debug("Recording ended successfully")
-
+                    self.launch_playwright(interpreter_info.get_python_exe())
                     return ActionResultDict(
                         {
                             "success": True,
                             "message": None,
-                            "result": output,
+                            "result": None,
                         }
                     )
         except Exception as e:
@@ -96,3 +74,49 @@ class _Playwright(object):
                 "result": None,
             }
         )
+
+    def launch_playwright(self, python_exe):
+        import os
+        import sys
+        import threading
+        import subprocess
+        from robocorp_ls_core.basic import build_subprocess_kwargs
+
+        cmd = [
+            python_exe,
+            os.path.join(os.path.dirname(__file__), "__playwright__main.py"),
+        ]
+
+        env = os.environ.copy()
+        kwargs: dict = build_subprocess_kwargs(None, env, stderr=subprocess.PIPE)
+        kwargs["stdout"] = subprocess.PIPE
+        kwargs["stderr"] = subprocess.PIPE
+
+        def on_output(content):
+            try:
+                sys.stderr.buffer.write(content)
+            except:
+                log.exception("Error reporting interactive output.")
+
+        def stream_reader(stream, callback):
+            try:
+                for line in iter(stream.readline, b""):
+                    callback(line)
+            except Exception as e:
+                log.error("Streaming failed:", e)
+
+        with subprocess.Popen(cmd, **kwargs) as process:
+            threads = [
+                threading.Thread(
+                    target=stream_reader,
+                    args=(process.stdout, on_output),
+                    name="stream_reader_stdout",
+                ),
+                threading.Thread(
+                    target=stream_reader,
+                    args=(process.stderr, on_output),
+                    name="stream_reader_stderr",
+                ),
+            ]
+            for t in threads:
+                t.start()
