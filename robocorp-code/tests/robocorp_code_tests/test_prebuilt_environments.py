b707e8cc4f9de76a626f73930a6a1c5a1289241e
Fabio Zadrozny <fabiofz@gmail.com>
2023-08-11 16:23:39 -0300

    Fix test.

--- robocorp-code/tests/robocorp_code_tests/test_prebuilt_environments.py
+++ robocorp-code/tests/robocorp_code_tests/test_prebuilt_environments.py
@@ -14,24 +14,37 @@ def test_prebuilt_environments(rcc: IRcc, tmpdir):
     import requests
     import robocorp_code
 
-    repo_root = Path(robocorp_code.__file__).parent.parent.parent
-    assert repo_root.name == "robocorp-code"
+    robocorp_code_root = Path(robocorp_code.__file__).parent.parent.parent
 
-    rcc_ts = repo_root / "vscode-client" / "src" / "rcc.ts"
+    rcc_ts = robocorp_code_root / "vscode-client" / "src" / "rcc.ts"
+    if not rcc_ts.exists():
+        # In GitHub we're in the site-packages.
+        import os
+
+        robocorp_code_root = Path(os.environ["GITHUB_WORKSPACE"]) / "robocorp-code"
+        if not robocorp_code_root.exists():
+            msg = [f"{robocorp_code_root} does not exist.", "Environment:"]
+            for key, val in sorted(os.environ.items()):
+                msg.append(f"{key}={val}")
+            raise AssertionError("\n".join(msg))
+        rcc_ts = robocorp_code_root / "vscode-client" / "src" / "rcc.ts"
+
     text = rcc_ts.read_text("utf-8")
-    conda_yaml = repo_root / "bin" / "create_env"
+    conda_yaml = robocorp_code_root / "bin" / "create_env"
 
     if sys.platform == "win32":
         check = "const BASENAME_PREBUILT_WIN_AMD64 = "
-        conda_yaml /= "conda_vscode_windows_amd64.yml"
+        conda_yaml /= "conda_vscode_windows_amd64.yaml"
     elif "linux" in sys.platform:
         check = "const BASENAME_PREBUILT_LINUX_AMD64 = "
-        conda_yaml /= "conda_vscode_linux_amd64.yml"
+        conda_yaml /= "conda_vscode_linux_amd64.yaml"
     elif sys.platform == "darwin":
         check = "const BASENAME_PREBUILT_DARWIN = "
-        conda_yaml /= "conda_vscode_darwin_amd64.yml"
+        conda_yaml /= "conda_vscode_darwin_amd64.yaml"
     else:
         raise AssertionError("Unexpected platform: {sys.platform}.")
+
+    assert conda_yaml.exists()
 
     # The code we're searching for is something as:
     # const BASENAME_PREBUILT_WIN_AMD64 = "978947424da5b5d4_windows_amd64.zip";
