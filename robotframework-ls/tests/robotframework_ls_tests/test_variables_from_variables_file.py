2769171a6b8a5f23716ea963fdbaaefe9548dd68
viktor1230 <74011369+viktor1230@users.noreply.github.com>
2024-03-21 07:23:19 +0100

    Update test_variables_from_variables_file.py

--- robotframework-ls/tests/robotframework_ls_tests/test_variables_from_variables_file.py
+++ robotframework-ls/tests/robotframework_ls_tests/test_variables_from_variables_file.py
@@ -1,7 +1,14 @@
 def test_variables_from_variables_file(tmpdir):
+    import typing
     from robotframework_ls.impl.variables_from_variable_file import (
         VariablesFromVariablesFileLoader,
     )
+    from robotframework_ls.impl.protocols import (
+        IRobotDocument,
+    )
+    from robocorp_ls_core import uris
+    from robocorp_ls_core.watchdog_wrapper import create_observer
+    from robocorp_ls_core.workspace import Workspace
 
     filename = tmpdir.join("my.txt")
     filename.write_text(
@@ -13,5 +20,10 @@ var2='value var2'
         encoding="utf-8",
     )
 
-    v = VariablesFromVariablesFileLoader(str(filename))
+    ws = Workspace(str(filename), create_observer("dummy", None))
+
+    doc_uri = uris.from_fs_path(str(filename))
+    resource_doc = ws.get_document(doc_uri, accept_from_file=True)
+    robot_doc = typing.cast(IRobotDocument, resource_doc)
+    v = VariablesFromVariablesFileLoader(str(filename), robot_doc)
     assert [x.variable_name for x in v.get_variables()] == ["V_NAME", "V_NAME1", "var2"]
