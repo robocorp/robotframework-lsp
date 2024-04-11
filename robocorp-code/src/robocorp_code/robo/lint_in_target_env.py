fee44df0cd1fde4efff0156cae00c483f323c9a6
Fabio Zadrozny <fabiofz@gmail.com>
2024-04-11 11:05:27 -0300

    Fix linting with robocorp-actions 0.2.0

--- robocorp-code/src/robocorp_code/robo/lint_in_target_env.py
+++ robocorp-code/src/robocorp_code/robo/lint_in_target_env.py
@@ -8,13 +8,40 @@ def main() -> None:
         # May not be in the target env or could be an older version of
         # robocorp.actions.
         from robocorp.actions import _lint_action  # noqa #type: ignore
+        from robocorp.actions import version_info  # noqa #type: ignore
+
+        requires_pm = version_info[:2] >= [0, 2]
     except BaseException:
         return
 
-    try:
-        errors = list(_lint_action.iter_lint_errors(contents_to_lint))
-    except BaseException:
-        return
+    pm = None
+    if requires_pm:
+        try:
+            from robocorp.actions._managed_parameters import (
+                ManagedParameters,  # type: ignore
+            )
+            from robocorp.tasks._customization._extension_points import (
+                EPManagedParameters,  # type: ignore
+            )
+            from robocorp.tasks._customization._plugin_manager import (
+                PluginManager,  # type: ignore
+            )
+
+            pm = PluginManager()
+            pm.set_instance(EPManagedParameters, ManagedParameters({}))
+        except BaseException:
+            pass
+
+    if requires_pm:
+        try:
+            errors = list(_lint_action.iter_lint_errors(contents_to_lint, pm=pm))
+        except BaseException:
+            return
+    else:
+        try:
+            errors = list(_lint_action.iter_lint_errors(contents_to_lint))
+        except BaseException:
+            return
 
     lst = []
     for error in errors:
